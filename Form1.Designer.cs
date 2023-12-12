491a0d82c0bab30278096f534183f0a7b2514f1a
Bernie <95599456+Unforgettableeternalproject@users.noreply.github.com>
2023-12-12 14:25:22 +0800

    First Commit: Initialize Components 12/12

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_enter = new System.Windows.Forms.Button();
+            this.labTest = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_enter
+            // 
+            this.btn_enter.BackColor = System.Drawing.Color.FloralWhite;
+            this.btn_enter.Font = new System.Drawing.Font("微軟正黑體", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn_enter.Location = new System.Drawing.Point(435, 264);
+            this.btn_enter.Name = "btn_enter";
+            this.btn_enter.Size = new System.Drawing.Size(415, 135);
+            this.btn_enter.TabIndex = 0;
+            this.btn_enter.Text = "進入";
+            this.btn_enter.UseVisualStyleBackColor = false;
+            this.btn_enter.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labTest
+            // 
+            this.labTest.AutoSize = true;
+            this.labTest.Font = new System.Drawing.Font("微軟正黑體", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labTest.Location = new System.Drawing.Point(607, 431);
+            this.labTest.Name = "labTest";
+            this.labTest.Size = new System.Drawing.Size(67, 34);
+            this.labTest.TabIndex = 1;
+            this.labTest.Text = "Test";
+            this.labTest.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1334, 697);
+            this.Controls.Add(this.labTest);
+            this.Controls.Add(this.btn_enter);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_enter;
+        private System.Windows.Forms.Label labTest;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,26 @@ namespace LBN_Competitive_System_Simulation
 {
     public partial class Form1 : Form
     {
+        public int test = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            labTest.Text = "Test" + test.ToString();
+            test++;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
