f5853018f921655334405a311e78688fcadcc137
Bernie <ptyc4076@gmail.com>
2023-12-21 22:57:40 +0800

    Fixed even more bugs, added a new screenshots and concept arts, updated README.md. 12/21

--- Forms/BrowseForm.cs
+++ Forms/BrowseForm.cs
@@ -38,7 +38,11 @@ namespace LBN_Competitive_System_Simulation
             "自己加分，自己加分",
             "有沒有人可以借我一件褲子?",
             "☺☺☺☺☺☺☺☺☺☺☺☺☺",
-            "喔是喔真的假的5555555"
+            "喔是喔真的假的5555555",
+            "???????????",
+            "急了",
+            "0",
+            "這不是肯德基"
         };
 
         private List<string> demoIDs = new List<string>
